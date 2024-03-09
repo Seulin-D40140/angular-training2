f52482d2d7f832e56e968fe244d5047fdd26a3ee
Donovan Seulin <nel640@hotmail.fr>
2024-03-09 09:46:40 +0100

    fix bug changement de formulaire (addbool)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserService } from './services/user.service';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,11 @@ import { UserService } from './services/user.service';
 })
 export class AppComponent {
 
-  constructor(public usersevice : UserService) {  }
+  constructor(public usersevice : UserService , private apiservice : ApiService) {  }
   title = 'trainings-front-app';
+
+  resetAddBool()
+	{
+	  this.apiservice.addBool = false
+	}
 }
